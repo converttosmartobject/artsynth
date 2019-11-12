bdf9c0ebac5faa873c0ef3ceb4b95f607fb3e92b
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-11-12 15:51:49 -0800

    x

--- sketch.js
+++ sketch.js
@@ -4,8 +4,11 @@ let px = 0;
 let py = 0;
 let easing = 0.1;
 let gif_createImg;
+let gif_createImg2;
+
 
 function preload() {
+  gif_createImg2 = createImg("images/down.gif");
   gif_createImg = createImg("images/squid.gif");
 }
 
@@ -16,18 +19,24 @@ function setup() {
 function draw() {
 if (mouseIsPressed) {
   strokeWeight(5)
+  stroke(0,0,255);
   line(mouseX, mouseY, pmouseX, pmouseY);
+  line(width - mouseX, height - mouseY, pmouseX, pmouseY);
 }
+
 gif_createImg.position(mouseX, mouseY/2);
+
   brush();
 }
 
 
 function brush(){
+  gif_createImg2.position(mouseX/1.2, mouseY/1.4);
 
   x += (mouseX - x) * easing;
   y += (mouseY - y) * easing;
   strokeWeight(15);
+  stroke(0);
   line(x, y, px, py);
 
   px = x;
@@ -35,5 +44,6 @@ function brush(){
 }
 
 function mousePressed(){
- background(220);
+ background(255);
+
 }
