e71d89bb4f7b77270a8d8aca03c7cd38fcd3e8f9
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-11-14 16:24:15 -0800

    n

--- sketch.js
+++ sketch.js
@@ -6,28 +6,71 @@ let easing = 0.1;
 let gif_createImg;
 let gif_createImg2;
 
+let osc;
+let pNoise;
+let music;
+let amplitude;
+let volHistory = [];
+
 
 function preload() {
   gif_createImg2 = createImg("images/down.gif");
   gif_createImg = createImg("images/squid.gif");
+    music = loadSound('underwater-bubbles.wav');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0, 135, 150);
+  osc = new p5.Oscillator('triangle');
+  amplitude = new p5.Amplitude();
+  // osc.setType('triangle');
+
 }
 
 function draw() {
-if (mouseIsPressed) {
-  strokeWeight(5)
-   // stroke(map(mouseY, 0, 600, 0, 255, true));
-   stroke(255);
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  line(width - mouseX, height - mouseY, pmouseX, pmouseY);
-}
+// if (mouseIsPressed) {
+//   strokeWeight(1)
+//    stroke(map(mouseY, 0, 600, 0, 255, true));
+//    noFill();
+//   ellipse(mouseX, mouseY, 10, 10);
+//   ellipse(width - mouseX, height - mouseY, 10, 10);
+// }
 
 gif_createImg.position(mouseX, mouseY/2);
 
   brush();
+
+  let level = amplitude.getLevel();
+
+  pNoise = noise(frameCount / 20) * 100;
+
+  osc.freq(map(mouseX, 0, width, 60, 1200) + pNoise);
+
+  osc.amp(map(sin(frameCount / 20), -1, 1, 0.05, .2));
+  // osc.amp(map(mouseY, 0, height, 0.2, 0));
+
+  volHistory.push(level);
+noFill();
+strokeWeight(1);
+stroke(242, 152, 116);
+beginShape();
+
+  for (let i = 0; i < volHistory.length; i++){
+    let y = map(volHistory[i], 0, 1, height, 0);
+    vertex(i, y);
+  }
+endShape();
+// stroke(252, 3, 244);
+stroke(255);
+  ellipse(mouseX, mouseY, level * 100, level * 100);
+  ellipse(mouseX+20, mouseY+20, level * 200, level * 200);
+  // ellipse(mouseX+40, mouseY+40, level * 300, level * 300);
+
+  if (volHistory.length > width - 50){
+    volHistory.splice(0,1);
+  }
+
 }
 
 
@@ -45,6 +88,12 @@ function brush(){
 }
 
 function mousePressed(){
- background(255);
+  background(0, 135, 150);
+  osc.start();
+  music.play();
+}
 
+function mouseReleased(){
+  osc.stop();
+  music.stop();
 }
